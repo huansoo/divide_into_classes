9ce98abeda870633676e62b66ac19a940e85c683
huansoo <991622230@qq.com>
2018-05-05 19:28:04 +0800

    init

---
+++ src/main/java/com/baidu/divide_into_classes/DivideIntoClassesApplication.java
@@ -0,0 +1,12 @@
+package com.baidu.divide_into_classes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivideIntoClassesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivideIntoClassesApplication.class, args);
+	}
+}
